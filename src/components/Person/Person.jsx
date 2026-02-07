48bb6b6afae8d856b3c28bf2939c1c3a846cc846
Vladyslav <vkobeluh@gmail.com>
2026-02-07 16:55:43 +0200

    fix solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,7 +11,11 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.age > 0 && (
+        <p className="Person__age">
+          I am {person.age}
+        </p>
+      )}
 
       <p className="Person__partner">{partnerMessage}</p>
     </section>
